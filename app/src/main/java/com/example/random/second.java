31f3c1c3086f81f83f34b332143358326060508a
Millin Khabiya <milinkhabya2000@gmail.com>
2020-10-15 15:07:31 +0530

    Back button added

--- app/src/main/java/com/example/random/second.java
+++ app/src/main/java/com/example/random/second.java
@@ -2,7 +2,10 @@ package com.example.random;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Arrays;
@@ -11,6 +14,7 @@ import java.util.Random;
 
 public class second extends AppCompatActivity {
     TextView name;
+    private Button btn;
 
     List<String> names = Arrays.asList("Milin", "Ram", "Shyam", "Mukesh");
     @Override
@@ -18,9 +22,18 @@ public class second extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+
         name = (TextView) findViewById(R.id.rand_name);
         Random rand = new Random();
         String randomName = names.get(rand.nextInt(names.size()));
         name.setText(randomName);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(second.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
